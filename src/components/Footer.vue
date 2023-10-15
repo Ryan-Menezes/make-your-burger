9a9860bada015957415f93ddf6222040b49673bc
Ryan de Menezes Nobre Ciriaco <menezesryan1010@gmail.com>
2023-10-14 23:00:48 -0300

    implementing vue router

---
+++ src/components/Footer.vue
@@ -0,0 +1,15 @@
+<template>
+  <footer>
+    <p>Footer</p>
+  </footer>
+</template>
+
+<script>
+  export default {
+    name: 'Footer'
+  }
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/components/Navbar.vue
@@ -0,0 +1,26 @@
+<template>
+  <header>
+    <nav id="nav">.
+      <RouterLink to="/">
+        <img src="" alt="" id="logo" />
+      </RouterLink>
+      <RouterLink to="/">Home</RouterLink>
+      <RouterLink to="/pedidos">Pedidos</RouterLink>
+    </nav>
+  </header>
+</template>
+
+<script>
+  import { RouterLink } from 'vue-router'
+
+  export default {
+    name: 'Navbar',
+    components: {
+      RouterLink
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
